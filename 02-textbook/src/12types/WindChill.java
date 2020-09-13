be1deea9eac4b8e8e561e8e63a6d0e2828a58cf2
binhbdn <binh.buidn@gmail.com>
2020-09-13 11:48:45 +0700

    Already understanding of WindChill.java class

---
+++ 02-textbook/src/12types/WindChill.java
@@ -0,0 +1,26 @@
+/******************************************************************************
+ *  https://introcs.cs.princeton.edu/java/12types/WindChill.java.html
+ *
+ *  Compilation:  javac WindChill.java
+ *  Execution:    java WindChill t v
+ *
+ *  Given the temperature t (in Fahrenheit) and the wind speed v
+ *  (in miles per hour), compute the wind chill w using the formula
+ *  from the National Weather Service.
+ *
+ *  w = 35.74 + 0.6215*t + (0.4275*t - 35.75)  *  v ^ 0.16
+ *
+ *  Reference:  http://www.nws.noaa.gov/om/windchill/index.shtml
+ *
+ ******************************************************************************/
+
+class WindChill {
+  public static void main(String[] args) {
+    double t = Double.parseDouble(args[0]);
+    double v = Double.parseDouble(args[1]);
+    double w = 35.74 + 0.6215*t + (0.4275*t - 35.75) * Math.pow(v, 0.16);
+    System.out.println("Temperature = " + t);
+    System.out.println("Wind speed  = " + v);
+    System.out.println("Wind chill  = " + w);
+  }
+}
