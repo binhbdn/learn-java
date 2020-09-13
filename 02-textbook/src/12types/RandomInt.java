640af60b234b926a9dc17dc5a49e883e442fffac
binhbdn <binh.buidn@gmail.com>
2020-09-13 11:42:29 +0700

    Already understanding of RandomInt.java class

---
+++ 02-textbook/src/12types/RandomInt.java
@@ -0,0 +1,37 @@
+/******************************************************************************
+ *  https://introcs.cs.princeton.edu/java/12types/RandomInt.java.html
+ *
+ *  Compilation:  javac RandomInt.java
+ *  Execution:    java RandomInt N
+ *
+ *  Prints a pseudo-random integer between 0 and N-1.
+ *  Illustrate an explicit type conversion (cast) from double to int.
+ *
+ *  % java RandomInt 6
+ *  Your random integer is: 5
+ *
+ *  % java RandomInt 6
+ *  Your random integer is: 0
+ *
+ *  % java RandomInt 1000
+ *  Your random integer is: 129
+ *
+ *  % java RandomInt 1000
+ *  Your random integer is: 333
+ *
+ ******************************************************************************/
+
+class RandomInt { 
+    public static void main(String[] args) { 
+        // a positive integer
+        int n = Integer.parseInt(args[0]);
+
+        // a pseudo-random real between 0.0 and 1.0
+        double r = Math.random(); 
+
+        // a pseudo-random integer between 0 and n-1
+        int value = (int) (r * n);
+
+        System.out.println(value);
+    }
+}
