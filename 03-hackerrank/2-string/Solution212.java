d6800514021851a77d7094309112344942af0783
binhbdn <binh.buidn@gmail.com>
2020-09-18 15:25:24 +0700

    Strings- completed all challenges

---
+++ 03-hackerrank/2-string/Solution212.java
@@ -0,0 +1,30 @@
+// https://www.hackerrank.com/challenges/tag-content-extractor
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+class Solution212 {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+		int testCases = Integer.parseInt(in.nextLine());
+		while(testCases>0){
+      String line = in.nextLine();
+      
+      boolean matchFound = false;
+      Pattern p = Pattern.compile("<(.+)>([^<]+)</\\1>");
+      Matcher m = p.matcher(line);
+
+      while (m.find()) {
+        System.out.println(m.group(2));
+        matchFound = true;
+      }
+      if ( ! matchFound) {
+        System.out.println("None");
+      }
+			
+			testCases--;
+    }
+    in.close();
+  }
+}
