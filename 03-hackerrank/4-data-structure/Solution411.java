c2296690914c6c5738696cff24cb3ba64b442758
binhbdn <binh.buidn@gmail.com>
2020-09-23 11:27:35 +0700

    array sorting by comparator

---
+++ 03-hackerrank/4-data-structure/Solution411.java
@@ -0,0 +1,46 @@
+// https://www.hackerrank.com/challenges/java-comparator
+
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Comparator;
+
+// Write your Checker class here
+class Checker implements Comparator<Player>{
+  public int compare(Player a, Player b) {
+    if (a.score == b.score){
+      return a.name.compareTo(b.name);//alphabetically
+    } else {
+      return b.score - a.score;//decreasing 
+    }
+  }
+}
+
+class Player{
+  String name;
+  int score;
+  
+  Player(String name, int score){
+    this.name = name;
+    this.score = score;
+  }
+}
+
+class Solution411 {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int n = scan.nextInt();
+
+    Player[] player = new Player[n];
+    Checker checker = new Checker();
+    
+    for(int i = 0; i < n; i++){
+      player[i] = new Player(scan.next(), scan.nextInt());
+    }
+    scan.close();
+ 
+    Arrays.sort(player, checker);
+    for(int i = 0; i < player.length; i++){
+      System.out.printf("%s %s\n", player[i].name, player[i].score);
+    }
+  }
+}
