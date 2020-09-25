94c66287464c64f3f36612a8bfaa5e67f6fbe0c3
binhbdn <binh.buidn@gmail.com>
2020-09-25 08:58:35 +0700

    math.BigDecimal

--- 03-hackerrank/3-big-number/Solution303.java
+++ 03-hackerrank/3-big-number/Solution303.java
@@ -5,7 +5,6 @@ import java.math.BigDecimal;
 
 class Solution303{
   public static void main(String []args){
-    //Input
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
     String[] s = new String[n+2];
