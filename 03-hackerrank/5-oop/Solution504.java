13860a1cf6249f21640b61d21c74072b0d051272
binhbdn <binh.buidn@gmail.com>
2020-09-25 14:37:33 +0700

    interface : fix bug Class[] -> Class<?>[]

--- 03-hackerrank/5-oop/Solution504.java
+++ 03-hackerrank/5-oop/Solution504.java
@@ -37,7 +37,7 @@ class Solution504{
   
   // ImplementedInterfaceNames method takes an object and prints the name of the interfaces it implemented
   static void ImplementedInterfaceNames(Object o){
-    Class[] theInterfaces = o.getClass().getInterfaces();
+    Class<?>[] theInterfaces = o.getClass().getInterfaces();
     for (int i = 0; i < theInterfaces.length; i++){
       String interfaceName = theInterfaces[i].getName();
       System.out.println(interfaceName);
