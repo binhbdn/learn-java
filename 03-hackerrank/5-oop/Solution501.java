de7f19c42e8d35ecf602fa64008b2b6ed0c2a533
binhbdn <binh.buidn@gmail.com>
2020-09-25 12:30:53 +0700

    inheritance 1

---
+++ 03-hackerrank/5-oop/Solution501.java
@@ -0,0 +1,19 @@
+// https://www.hackerrank.com/challenges/java-inheritance-1
+
+class Animal{
+	void walk(){System.out.println("I am walking");}
+}
+
+class Bird extends Animal{
+	void fly(){System.out.println("I am flying");}
+	void sing(){System.out.println("I am singing");}
+}
+
+class Solution501{
+  public static void main(String args[]){
+    Bird bird = new Bird();
+    bird.walk();
+    bird.fly();
+    bird.sing();
+  }
+}
