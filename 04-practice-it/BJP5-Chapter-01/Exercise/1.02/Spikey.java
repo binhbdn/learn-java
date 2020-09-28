88d82b0f18a00456955dc87860ee9d05338fae71
binhbdn <binh.buidn@gmail.com>
2020-09-28 15:04:20 +0700

    BJP5 Exercise 1.2: Spikey

---
+++ 04-practice-it/BJP5-Chapter-01/Exercise/1.02/Spikey.java
@@ -0,0 +1,22 @@
+/*
+https://practiceit.cs.washington.edu/problem/view/bjp5/chapter1/e2-Spikey
+BJP5 Exercise 1.2: Spikey
+
+Write a complete Java program in a class named Spikey that prints the following output:
+  \/
+ \\//
+\\\///
+///\\\
+ //\\
+  /\
+*/
+public class Spikey{
+    public static void main(String[] args){
+        System.out.println("  \\/");
+        System.out.println(" \\\\//");
+        System.out.println("\\\\\\///");
+        System.out.println("///\\\\\\");
+        System.out.println(" //\\\\");
+        System.out.println("  /\\");
+    }
+}
