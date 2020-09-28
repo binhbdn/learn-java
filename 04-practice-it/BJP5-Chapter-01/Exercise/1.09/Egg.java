0038c1fad6139e6d4f8de92bf2a69b77d7e0ee12
binhbdn <binh.buidn@gmail.com>
2020-09-28 16:11:15 +0700

    BJP5 Exercise 1.9: Egg

---
+++ 04-practice-it/BJP5-Chapter-01/Exercise/1.09/Egg.java
@@ -0,0 +1,22 @@
+/*
+https://practiceit.cs.washington.edu/problem/view/bjp5/chapter1/e9-Egg
+BJP5 Exercise 1.9: Egg
+
+Write a complete Java program in a class named Egg that displays the following output:
+  _______
+ /       \
+/         \
+-"-'-"-'-"-
+\         /
+ \_______/
+*/
+public class Egg{
+    public static void main(String[] args){
+        System.out.println("  _______");
+        System.out.println(" /       \\");
+        System.out.println("/         \\");
+        System.out.println("-\"-'-\"-'-\"-");
+        System.out.println("\\         /");
+        System.out.println(" \\_______/");
+    }
+}
