8bca932fb7b9a13501ebbed8b368ceb8c6aa1bc5
binhbdn <binh.buidn@gmail.com>
2020-09-28 15:03:50 +0700

    BJP5 Exercise 1.1: Stewie

---
+++ 04-practice-it/BJP5-Chapter-01/Exercise/1.01/Stewie.java
@@ -0,0 +1,16 @@
+/*
+https://practiceit.cs.washington.edu/problem/view/bjp5/chapter1/e1-Stewie
+BJP5 Exercise 1.1: Stewie
+
+Write a complete Java program in a class named Stewie that prints the following output:
+//////////////////////
+|| Victory is mine! ||
+\\\\\\\\\\\\\\\\\\\\\\
+*/
+public class Stewie{
+    public static void main(String[] args){
+        System.out.println("//////////////////////");
+        System.out.println("|| Victory is mine! ||");
+        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+    }
+}
