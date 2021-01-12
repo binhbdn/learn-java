bac7a649ae8bd2b1e05a63896f53a0b07606d480
binhbdn <binh.buidn@gmail.com>
2021-01-12 10:00:21 +0700

    Asm4_binhbdnFX06635: ICalculator.java

---
+++ 05-funix/PRO192x_02_VN/asm4/humanresources/ICalculator.java
@@ -0,0 +1,6 @@
+/* PRO192x_02_VN_Asm4_binhbdnFX06635 */
+
+// ICalculator là giao diện chứa hàm tính lương
+public interface ICalculator {
+    double calculateSalary(); // phương thức giao diện theo mặc định abstract và public
+}
